95c2beda658a0916bc4f5f43fa3ab9bc19ee502a
Azat <ergeshbaevazat@gmail.com>
2023-09-12 16:58:40 +0300

    first commit

---
+++ src/components/todo/Todo.tsx
@@ -0,0 +1,69 @@
+import { FC, useState } from "react";
+import { useDispatch } from "react-redux";
+import { AppDispatch } from "../../redux";
+import { deleteTodo, onEditTodo, onStatusChange } from "../../redux/todoSlice";
+import { TodoType } from "../../types";
+import css from "./Todo.module.css";
+
+const Todo: FC<TodoType> = (props) => {
+  const [isEdit, setEdit] = useState(false);
+  const [inputValue, setInputValue] = useState(props.title);
+
+  const dispatch = useDispatch<AppDispatch>();
+  const handleEdit = () => {
+    setEdit(!isEdit);
+  };
+
+  const handleDelete = () => {
+    dispatch(deleteTodo(props.id));
+  };
+
+  const handleStatus = () => {
+    dispatch(onStatusChange(props.id));
+  };
+
+  const handleInput = (e) => {
+    setInputValue(e.target.value);
+  };
+
+  const submit = (e) => {
+    e.preventDefault();
+    dispatch(onEditTodo({ id: props.id, inputValue }));
+    setEdit(false);
+  };
+
+  return (
+    <div className={css.wrapper}>
+      {isEdit ? (
+        <form onSubmit={submit}>
+          <input
+            value={inputValue}
+            className="editInput"
+            onChange={handleInput}
+            type="text"
+          />
+          <button className="editBtn">Save</button>
+        </form>
+      ) : (
+        <label>
+          <input
+            checked={props.status}
+            onChange={handleStatus}
+            type="checkbox"
+          />
+          <p className={props.status ? css.compleat : ""}>{props.title}</p>
+        </label>
+      )}
+      <div>
+        <button onClick={handleEdit} className="mainBtn">
+          Edit
+        </button>
+        <button onClick={handleDelete} className="mainBtn">
+          Del
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Todo;
